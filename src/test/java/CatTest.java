5f90cd75b5204f3023acff51052f12da404de0f3
cinanna <cinnabarworld@gmail.com>
2023-02-03 22:53:36 +0700

    add jacoco report

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,4 +1,28 @@
-import static org.junit.Assert.*;
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-  
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundTest() {
+        String expectedResult = "Мяу";
+        Cat cat = new Cat(feline);
+        String testResult= cat.getSound();
+        Assert.assertEquals(expectedResult, testResult);
+    }
+    @Test
+    public void getFoodTest() throws Exception{
+        Cat cat = new Cat(feline);
+        List<String> result = cat.getFood();
+        Assert.assertTrue(result.isEmpty());
+    }
 }
